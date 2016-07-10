5b16648747fc25e2975f9250509e051c57ae41b8
Sawyer-Powell <sawyerhpowell@gmail.com>
2016-07-10 16:47:35 -0400

    Added buttons to ViewController.swift, Created dictionary matching terms with images, created array that will store the items in the schedule

--- AutismSchedule/ViewController.swift
+++ AutismSchedule/ViewController.swift
@@ -9,6 +9,73 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var dict_Images: [String:String] = [
+        "Strawberry":"Strawberry.JPG",
+        "Socks On":"Socks On.JPG",
+        "Cleaning":"Cleaing.jpg",
+        "WiiMote":"WiiMote.JPG",
+        "Water":"Water.JPG",
+        "Washing and Drying":"Washing and Drying.JPG",
+        "Toothbrush":"Toothbrush.JPG",
+        "Shoes On": "Shoes On.JPG",
+        "Puzzles":"Puzzles.JPG",
+        "Plate":"Plate.JPG",
+        "Orange":"Orange.JPG",
+        "Math":"Math.JPG",
+        "Brush":"Brush.JPG",
+        "Hand Wash":"HandWash.JPG",
+        "Bread":"Bread.JPG",
+        "Checkers":"Checkers.JPG",
+        "Book Read":"BookRead.JPG",
+        "Eat Fruit":"Eat Fruit.JPG",
+        "Basketball":"Basketball.JPG",
+        "Computer":"Computer.JPG"
+        
+    ]
+    var arr_Schedule: [String] = [""]
+    
+    @IBAction func btn_Strawberry(sender: UIButton) {
+        
+    }
+    @IBAction func btn_socksOn(sender: UIButton) {
+    }
+    @IBAction func btn_Cleaning(sender: UIButton) {
+    }
+    @IBAction func btn_WiiMote(sender: UIButton) {
+    }
+    @IBAction func btn_Water(sender: UIButton) {
+    }
+    @IBAction func btn_WashingDrying(sender: UIButton) {
+    }
+    @IBAction func btn_Toothbrush(sender: UIButton) {
+    }
+    @IBAction func btn_ShoesOn(sender: UIButton) {
+    }
+    @IBAction func btn_Puzzles(sender: UIButton) {
+    }
+    @IBAction func btn_Plate(sender: UIButton) {
+    }
+    @IBAction func btn_Orange(sender: UIButton) {
+    }
+    @IBAction func btn_Math(sender: UIButton) {
+    }
+    @IBAction func btn_Brush(sender: UIButton) {
+    }
+    @IBAction func btn_HandWash(sender: UIButton) {
+    }
+    @IBAction func btn_Bread(sender: UIButton) {
+    }
+    @IBAction func btn_Checkers(sender: UIButton) {
+    }
+    @IBAction func btn_BookRead(sender: UIButton) {
+    }
+    @IBAction func btn_Banana(sender: UIButton) {
+    }
+    @IBAction func btn_Basketball(sender: UIButton) {
+    }
+    @IBAction func btn_Computer(sender: UIButton) {
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
